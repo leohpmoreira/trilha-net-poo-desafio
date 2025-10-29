f1768926626f5aade535f6bbfd698e9cb7782bc7
leohpmoreira <90646838+leohpmoreira@users.noreply.github.com>
2025-10-29 02:07:40 -0300

    feat: implementar propriedades e herança nas classes Smartphone, Nokia e Iphone
    - Implementadas as propriedades e o construtor da classe abstrata Smartphone conforme o diagrama
    - Implementada a herança das classes Nokia e Iphone a partir de Smartphone
    - Sobrescrito o método InstalarAplicativo() em ambas as classes conforme o diagrama
    - Adicionados testes no Program.cs para validar as implementações

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("iPhone:");
+var iphoneTest = new Iphone("91234-5678", "iPhone 16 Pro Max", "213553049572979", 16);
+iphoneTest.Ligar();
+iphoneTest.ReceberLigacao();
+iphoneTest.InstalarAplicativo("YouTube");
+
+Console.WriteLine("\nNokia:");
+var nokiaTest = new Nokia("99876-5432", "Nokia 3310", "306877799098181", 32);
+nokiaTest.Ligar();
+nokiaTest.ReceberLigacao();
+nokiaTest.InstalarAplicativo("Facebook");
